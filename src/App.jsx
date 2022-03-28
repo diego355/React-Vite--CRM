c92249e130ad5b45b8ade9e5b24a76c4b4c89652
Diego Rojas <d.rojas@hermes-soft.com>
2022-03-27 21:37:30 -0600

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './dist/output.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-3xl'>CRM React</h1>
+    </div>
+  )
+}
+
+export default App
