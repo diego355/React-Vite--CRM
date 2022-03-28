84a035c8f7e283abf429a7f7a5efdb4d5c2385df
Diego Rojas <d.rojas@hermes-soft.com>
2022-03-28 13:18:25 -0600

    Layout y menú
    Estructura básica del Layout, se crea la barra de navegación y se implementan links con Link

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,31 @@
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 
 const Layout = ({}) => {
 
-    return(
-        <div>
-            <p>Layout...</p>
+    const location = useLocation()
+    const urlpath = location.pathname
 
-            <Outlet />
+    return(
+        <div className="md:flex md:min-h-screen">
+            <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+                <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+                <nav className="mt-10">
+                    <Link 
+                        className={`${ urlpath === '/clientes' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+                        to="/clientes"
+                    >Clientes</Link>
+                    <Link 
+                        className={`${ urlpath === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+                        to="/clientes/nuevo"
+                    >Nuevo Cliente</Link>
+                </nav>
+            </div>
+            <div className="md:w-3/4">
+                Columna 2 Outlet (3/4)
+                <Outlet />
+            </div>
+            
         </div>
     )
 }
